981a0cbc27236446701c9e22df59b5de3f40108d
Flusco <hdonaireandreu@gmail.com>
2026-02-24 07:56:37 -0100

    Primer commit

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let name= "Guillermo";
+    let age=23;
+    println!("El seu nom és {} i té {} anys",name,age);
+
+}
---
+++ target/debug/deps/DonaireHugo_GiliPol_VallesOriol_TV1-3e61c192e5262275.d
@@ -0,0 +1,5 @@
+/home/flusco/BINFO_UDA/BINFO_2/M7/Cryptography/DonaireHugo_GiliPol_VallesOriol_TV1/target/debug/deps/DonaireHugo_GiliPol_VallesOriol_TV1-3e61c192e5262275.d: src/main.rs
+
+/home/flusco/BINFO_UDA/BINFO_2/M7/Cryptography/DonaireHugo_GiliPol_VallesOriol_TV1/target/debug/deps/DonaireHugo_GiliPol_VallesOriol_TV1-3e61c192e5262275: src/main.rs
+
+src/main.rs:
